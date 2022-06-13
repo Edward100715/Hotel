65f56047e1c58bfe04d232dd571204ac66295fb4
Edward Bermudez <edwardbermudez103@gmail.com>
2022-06-13 07:56:15 -0500

    V2.0. ....

---
+++ JS/hotel.js
@@ -0,0 +1,9 @@
+
+function Img1(){
+    const miImg1 = document.getElementById("habitacion");
+    miImg1.src = "../IMG/habitacion2.jpg"
+}
+function Img2(){
+    const miImg1 = document.getElementById("habitacion");
+    miImg1.src = "../IMG/habitacion1.jpg"
+}
