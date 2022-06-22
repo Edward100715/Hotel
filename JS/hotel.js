1da05f4dc75963edfac159aaf69cde025552d191
Edward Bermudez <edwardbermudez103@gmail.com>
2022-06-22 06:38:56 -0500

    4.0 Ok GitHub

--- JS/hotel.js
+++ JS/hotel.js
@@ -7,3 +7,14 @@ function Img2(){
     const miImg1 = document.getElementById("habitacion");
     miImg1.src = "../IMG/habitacion1.jpg"
 }
+
+function Cotizar() {
+    const nPers = document.querySelector("#nroPer").value;
+    const nDias = document.querySelector("#nroDias").value;
+    const nValorPers = document.querySelector("#vlrPer").value;
+    const nResultado = document.querySelector("#Resultado");
+
+    const nTotal = Number(nPers) * Number(nDias) * Number(nValorPers);
+
+    nResultado.textContent = nTotal;
+}
